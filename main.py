55c6ff4b8f7fb11ac7783f842d6a271dfb72b7c8
Naokun <70059960+Nao-kun81@users.noreply.github.com>
2022-09-06 22:23:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from message import change_bright, check_tesuto_onoff, schedule, scope, kanji, b
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('BL7KSFCMl1n82l2FTjd12SdXs1O9wS9zmzTRQDSVoKjssrgBdojh94pRRhRDo4lEeYqsAhsJXH9r4d39k5J71dLtaFlFM+B4jqxqCYWARsISf81w5SYkfg1IOChpESbWYG+LoXlo++t3ag2Pdwq6twdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('4a7d64faa1fb0e1edf7eb2f7e6c92e94')
+line_bot_api = LineBotApi('チャンネルアクセストークン')
+handler = WebhookHandler('シークレットキー')
 
 
 @app.route("/callback", methods=['POST'])
@@ -189,11 +189,6 @@ def handle_message(event):
 if __name__ == "__main__":
     app.run(debug=True)
 
-'''
-開発日記(2022年09月06日)
-
-最後にreseet_schedule.pyを別フォルダにしてprocfileを作成、herokuで別々のプロジェクトとしてデプロイ)
-'''
 
 '''
 $env:FLASK_ENV = "development"
